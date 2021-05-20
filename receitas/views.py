930f716045a8ebf46360e45a5abe6696b466ef64
Katlen <katlen.cost@gmail.com>
2021-05-20 01:28:37 -0300

    commit changes

--- receitas/views.py
+++ receitas/views.py
@@ -5,7 +5,19 @@ from django.http import HttpResponse
 
 
 def index(response):
-    return render(response, 'index.html')
+
+    receitas = {
+        1: 'Lasanha',
+        2: 'Sopa de legumes',
+        3: 'Sorvete',
+        4: 'Bolo de chocolate',
+    }
+
+    dados = {
+        'nome_das_receitas': receitas
+    }
+
+    return render(response, 'index.html', dados)
 
 
 def receita(response):
